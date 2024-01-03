d18c2720a554cc0f398a6ba20fcba6dae45bc368
wormhole2121 <stefano.grandi1995@gmail.com>
2024-01-03 16:26:38 +0100

    Test

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -3,12 +3,16 @@
     <div class="carousel-inner">
         <div class="carousel-item active">
             <video autoplay loop muted playsinline>
+                {{-- 4k --}}
                 <source src="media/video-1.mp4" type="video/mp4">
+                {{-- 1080p --}}
+                <!-- Sorgente a risoluzione standard o più bassa -->
+                <source src="media/video-2.mp4" type="video/mp4">
                 Your browser does not support the video tag.
-                
+
             </video>
         </div>
-      
+
     </div>
 
 </div>
--- resources/views/components/chiSono.blade.php
+++ resources/views/components/chiSono.blade.php
@@ -1,7 +1,6 @@
-
 <div class="container">
     <div class="row">
-    <h2 class="text-center titolo">•SINCE 09•</h2>
+        <h2 class="text-center titolo">•SINCE 09•</h2>
     </div>
 </div>
 
@@ -9,9 +8,14 @@
     <div class="row justify-content-between">
         <div class="col-12 col-md-6 col-sm-6 my-5">
             <p class="chi-sono-p my-3 ms-2">
-                La mia passione per il mondo della barberia è nata fin da giovane e si è trasformata in una professione dopo anni di studi e apprendistato. Nonostante la mia giovane età, ho accumulato un'esperienza significativa nel settore, imparando dalle diverse culture di immagine e bellezza europee.
+                La mia passione per il mondo della barberia è nata fin da giovane e si è trasformata in una professione
+                dopo anni di studi e apprendistato. Nonostante la mia giovane età, ho accumulato un'esperienza
+                significativa nel settore, imparando dalle diverse culture di immagine e bellezza europee.
                 <br><br>
-                Il mio salone, Giochi di Capelli, continua a evolversi per rimanere all'avanguardia, sempre pronto a offrire innovazione e personalizzazione in ogni acconciatura. Sono appassionato di stare al passo con le ultime tendenze e tecniche, garantendo che i miei clienti ricevano sempre il meglio in termini di stile e cura dei capelli.
+                Il mio salone, Giochi di Capelli, continua a evolversi per rimanere all'avanguardia, sempre pronto a
+                offrire innovazione e personalizzazione in ogni acconciatura. Sono appassionato di stare al passo con le
+                ultime tendenze e tecniche, garantendo che i miei clienti ricevano sempre il meglio in termini di stile
+                e cura dei capelli.
             </p>
         </div>
         <div class="col-12 col-md-6 col-sm-6 my-3">
