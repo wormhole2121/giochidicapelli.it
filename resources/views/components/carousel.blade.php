f40968bb1b482265bb2a60adab0714cab810344e
wormhole2121 <stefano.grandi1995@gmail.com>
2024-01-03 19:59:32 +0100

    test

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -2,14 +2,12 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <video autoplay loop muted playsinline>
+            <video autoplay loop muted playsinline preload="auto">
                 {{-- 4k --}}
                 <source src="media/video-1.mp4" type="video/mp4">
                 {{-- 1080p --}}
-                <!-- Sorgente a risoluzione standard o più bassa -->
-                <source src="media/video-2.mp4" type="video/mp4">
-                Your browser does not support the video tag.
-
+                {{-- <source src="media/video-2.mp4" type="video/mp4">
+                Your browser does not support the video tag --}}
             </video>
         </div>
 
