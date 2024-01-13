de209fac75c1861d78fd412405f04f1bdc0410b4
wormhole2121 <stefano.grandi1995@gmail.com>
2024-01-13 17:52:27 +0100

    maps

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-6">
                 <h3 class="nav-link">Dove trovarmi</h3>
                 <div style="max-width: 100%; overflow: hidden;">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d89248.16614985814!2d9.448942206844993!3d45.650717482171345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d45.7039232!2d9.5560397!4m5!1s0x4786b26ee6334647%3A0xc0d6e97389de06f8!2sGiochi%20Di%20Capelli%2C%20Via%20Monsignor%20Caccia%20Dominioni%2C%20Cornate%20d&#39;Adda%2C%20MB!3m2!1d45.651001699999995!2d9.463917499999999!5e0!3m2!1sit!2sit!4v1704626783269!5m2!1sit!2sit" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2788.992631737477!2d9.461369776614543!3d45.65096972095726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786b26ee6334647%3A0xc0d6e97389de06f8!2sGiochi%20Di%20Capelli!5e0!3m2!1sit!2sit!4v1705164462914!5m2!1sit!2sit" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
 
